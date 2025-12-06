dd2b66262c88ddee6bae0e451158bddf42259aa0
Piyush Channawar <channawarpiyush@gmail.com>
2025-12-06 21:26:17 +0530

    Initialize backend with Express server and essential dependencies

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
